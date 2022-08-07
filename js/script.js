1e9b654018307ff7efdc00419b4cf50f19c89ad2
Lucas Zamora <Lucaszam00@hotmail.com>
2022-08-07 19:50:09 -0300

    mil cambios

---
+++ js/script.js
@@ -0,0 +1,14 @@
+$(".option").click(function () {
+    $(".option").removeClass("active")
+    $(this).addClass("active")
+  });
+
+
+  const inicio= document.getElementById("inicio")
+  const sucursales= document.getElementById("inicio")
+  const tienda= document.getElementById("inicio")
+  const work= document.getElementById("inicio")
+
+
+
+  
